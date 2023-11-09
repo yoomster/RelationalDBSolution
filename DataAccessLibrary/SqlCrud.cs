2a7badf82e2bf6a4be45c92e4b07b85eab265cc0
naomifleurnoel <nperenboom@hotmail.com>
2023-11-09 16:03:19 +0100

    Added files for setting up connection to database

---
+++ DataAccessLibrary/SqlCrud.cs
@@ -0,0 +1,24 @@
+﻿using DataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class SqlCrud
+    {
+        private readonly string _connectionString;
+
+        public SqlCrud(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<BasicContactModel> GetAllContacts()
+        {
+
+        }
+    }
+}
--- SQLServerUI/Program.cs
+++ SQLServerUI/Program.cs
@@ -1,2 +1,29 @@
 ﻿
-Console.ReadLine();
+using Microsoft.Extensions.Configuration;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.ReadLine();
+
+
+    }
+
+    //unit testing would be great here: test if the connection string is returned
+    private static string GetConnectionString(string connectionStringName = "Default")
+    {
+        string output = "";
+
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+        var config = builder.Build();
+
+        output = config.GetConnectionString(connectionStringName);
+
+        return output;
+        }
+}
